89db42d0baa2a5ed1d24938f13bf9c926ed52a8d
MJ <MJaSchnee@gmail.com>
2020-04-16 16:54:19 -0400

    Got better instructions for what to do for bot

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 Discord bot for a Dungeons and Dragons server
 
 Current goal: 
-    Randomly select a weather condition
-    Send that weather condition out to all RP channels
+    User selects weather condition
+    Random description of weather condition is sent out to all RP channels
     Each RP channel will get a different message depending on the type of are it is (indoor vs outdoor)
--- test.js
+++ test.js
@@ -3,21 +3,15 @@ const testWeather = require('./testWeather.json');
 const testLocation = require('./testLocations.json');
 const numberOfLocations = Object.keys(testLocation).length;
 
-// Gets random weather condition
+// Random JSON object picker
 // Thanks to https://stackoverflow.com/questions/2532218/pick-random-property-from-a-javascript-object
-function getRandomWeather() {
-    var keys = Object.keys(testWeather);
-    var randomCondition = keys[ keys.length * Math.random() << 0 ];
 
-    console.log("Random condition: "+randomCondition+"\n");
-    return testWeather[randomCondition];
-}
 
-var randomCondition = getRandomWeather();
+var condition = "Drizzling"
 
 for(var i=0; i<numberOfLocations; i++) {
     var locationKeys = Object.keys(testLocation);
-    var channel = locationKeys[testLocationsKeys[i]];
+    var channel = testLocation[locationKeys[i]];
     console.log("Posting to channel: "+channel.channelID);
-    console.log("Condition: "+randomCondition[channel.type]+"\n");
+    console.log("Condition: "+testWeather[condition][channel.type]+"\n");
 }
