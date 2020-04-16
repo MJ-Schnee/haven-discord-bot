974a29c1738ca7b841161735ba5cd8a505b1b5ed
MJ <MJaSchnee@gmail.com>
2020-04-16 17:21:59 -0400

    Fixed to meet new requirements; Approved by Alex

--- test.js
+++ test.js
@@ -1,17 +1,22 @@
 // JSON files
-const testWeather = require('./testWeather.json');
-const testLocation = require('./testLocations.json');
-const numberOfLocations = Object.keys(testLocation).length;
+const weatherJSON = require('./testWeather.json');
+const channelsJSON = require('./testChannels.json');
+
+const condition = "Pouring"
 
 // Random JSON object picker
 // Thanks to https://stackoverflow.com/questions/2532218/pick-random-property-from-a-javascript-object
+var randomProperty = function (obj) {
+    var keys = Object.keys(obj);
+    return obj[keys[ keys.length * Math.random() << 0]];
+};
 
-
-var condition = "Drizzling"
-
-for(var i=0; i<numberOfLocations; i++) {
-    var locationKeys = Object.keys(testLocation);
-    var channel = testLocation[locationKeys[i]];
-    console.log("Posting to channel: "+channel.channelID);
-    console.log("Condition: "+testWeather[condition][channel.type]+"\n");
-}
+// Loop through all channels
+Object.keys(channelsJSON).forEach(channel => {
+    channelRef = channelsJSON[channel];
+    console.log(`=== ${channel} ===`);
+    console.log(`Type: ${channelRef.type}`);
+    console.log(`ID: ${channelRef.id}`);
+    console.log(`Weather: ${randomProperty(weatherJSON[condition][channelRef.type])}`);
+    console.log();
+});
