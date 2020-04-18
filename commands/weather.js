38598ee79dc34a6c1e4eb27efc14d8a012426c57
MJ <MJaSchnee@gmail.com>
2020-04-17 20:11:36 -0400

    Added ability to delete weather channels

--- commands/weather.js
+++ commands/weather.js
@@ -10,6 +10,33 @@ client.login(botToken);
 const channelsJSON = require('../test code/testChannels.json');
 const weatherJSON = require('../test code/testWeather.json');
 
+// Writes the JSON data from the Node server onto the local files
+const updateLocalJSONs = () => {
+    console.log(`Before:\n${channelsJSON}`);
+    // Node file system
+    const fs = require('fs');
+    // Write into the channels JSON
+    fs.writeFile('./test code/testChannels.json', JSON.stringify(channelsJSON, null, 4), 'utf8', error => { 
+        if(error) 
+            console.error(error);
+        console.log("Local file write successful for testChannels.json");
+    });
+    // Write into the weather JSON
+    fs.writeFile('./test code/testWeather.json', JSON.stringify(weatherJSON, null, 4), 'utf8', error => { 
+        if(error) 
+            console.error(error);
+            console.log("Local file write successful for testWeather.json");
+    });
+    console.log(`After:\n${channelsJSON}\n\n`);
+};
+
+// Random JSON object picker
+// Thanks to https://stackoverflow.com/questions/2532218/pick-random-property-from-a-javascript-object
+const randomProperty = obj => {
+    var keys = Object.keys(obj);
+    return obj[keys[ keys.length * Math.random() << 0]];
+};
+
 module.exports = {
     name: 'weather',
     description: 'Send Haven weather update to all text channels',
@@ -22,34 +49,32 @@ module.exports = {
             if(!weatherJSON[weather])
                 return message.reply("that weather type doesn't have any descriptions!");
 
-            // Random JSON object picker
-            // Thanks to https://stackoverflow.com/questions/2532218/pick-random-property-from-a-javascript-object
-            let randomProperty = function (obj) {
-                var keys = Object.keys(obj);
-                return obj[keys[ keys.length * Math.random() << 0]];
-            };
-
             // Send messages to all the channels
             Object.keys(channelsJSON).forEach(channelKey => {
                 const channelRef = channelsJSON[channelKey];
                 const weatherDescription = randomProperty(weatherJSON[weather][channelRef.type]);
                 // Fetches the channel based on its ID then sends the weather message to that channel
-                client.channels.fetch(channelRef.id).then(
-                    weatherChannel => { 
-                        weatherChannel.send(weatherDescription) 
-                        console.log(`Messaged ${channelKey} the ${channelRef.type} weather: ${weatherDescription}`);
-                    });
+                try {
+                    client.channels.fetch(channelRef.id).then(
+                        weatherChannel => { 
+                            weatherChannel.send(weatherDescription) 
+                            console.log(`Messaged ${channelKey} the ${channelRef.type} weather: ${weatherDescription}`);
+                        });
+                }
+                catch (error) { // Most likely the bot doesn't have access to that channel
+                    console.error(error);
+                    message.reply(`An error occurred trying to message the "${channelKey}" channel`);
+                };
             });
 
             return message.channel.send("The weather has been announced!");channelRef.type
         }
 
-        // Checks if adding a channel to send weather into
+        // Checks if adding a channel to send weather in
 		else if(args[0] == "add" && args[2]) {
             // Validate arguments
             if(message.mentions.channels.first().type == "text" && // Checks if text channel is mentioned
                 (args[2]=="outside" ||  args[2]=="inside")) { // Checks if inside/outside channel is specified
-
                     const channelName = message.mentions.channels.first().name;
                     const channelType = args[2];
                     const channelID = message.mentions.channels.first().id;
@@ -58,18 +83,24 @@ module.exports = {
                         "type": channelType.toString(),
                         "id": channelID.toString()
                     };
-
-                    console.log(channelsJSON);
-                    
-                    // File writing isn't working but it saves so long as the bot stays open
-                    const fs = require('fs');
-                    fs.writeFile('./test code/testChannels.json', JSON.stringify(channelsJSON, null, 4), 'utf8', error => { 
-                        if(error) 
-                            console.error(error);
-                        console.log("Channel added");
-                    });
+                
+                    updateLocalJSONs();
 
                     return message.channel.send(`${channelName} has been added to the list of weather channels!`);
+            }
+        }
+
+        // Checks if removing a channel to send weather in
+        else if(args[0] == "remove" && args[1]) {
+            // Validate arguments
+            if(message.mentions.channels.first().type == "text") {
+                const channelName = message.mentions.channels.first().name;
+
+                delete channelsJSON[channelName.toString()];
+
+                updateLocalJSONs();
+
+                return message.channel.send(`${channelName} has been removed from the list of weather channels!`);
             }
         }
 
